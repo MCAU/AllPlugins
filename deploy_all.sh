29eb2e59dff7e058587dc02979f09c6331b62f0b
Ryan McCue <ryanmccue@cubegames.net>
2010-12-08 01:35:30 +1000

    Include git commands in deploy_all.sh

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-for plugin in `ls -d */`; do echo $plugin; cd $plugin; ant deploy; cd ..; done
+# Make sure we're up-to-date first
+git pull
+git submodule update --init *
 
+for plugin in `ls -d */`; do echo $plugin; cd $plugin; ant deploy; cd ..; done
