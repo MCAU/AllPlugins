72fb211faa7fa4a37c58e3c3b76a18a40725d524
Ryan McCue <ryanmccue@cubegames.net>
2010-12-08 15:37:18 +1000

    Add ability to use custom build scripts

---
+++ builders/README.md
@@ -0,0 +1,3 @@
+Files in here will be copied to their relevant directories by deploy_all.sh
+
+Use these to override plugins' build.xml files for MCAU specific things.
--- deploy_all.sh
+++ deploy_all.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 # Make sure we're up-to-date first
-git pull
-git submodule update --init *
+#git pull
+#git submodule update --init *
 
-for plugin in `ls -d */`; do echo $plugin; cd $plugin; ant deploy; cd ..; done
+for plugin in `ls -d */`
+do
+	if [ -f builders/${plugin%/}.xml ]
+	then
+		echo "Using custom build script"
+		[ -f $plugin/build.xml ] && mv $plugin/build.xml $plugin/build.xml.old
+		cp builders/${plugin%/}.xml $plugin/build.xml
+		CUSTOM=true
+	fi
+	echo $plugin
+	cd $plugin
+	#ant deploy
+	if [ $CUSTOM ]
+	then
+		# We do this so that we don't leave our custom build.xml
+		# if there wasn't already one.
+		echo "Removing custom build script"
+		rm build.xml
+		[ -f build.xml.old ] && mv build.xml.old build.xml
+	fi
+	cd ..
+done
